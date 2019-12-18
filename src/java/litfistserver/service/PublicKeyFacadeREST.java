83a03efc36b7e3576a4d5e4955a05bcd92715b55
Carlos <carlosrafael-mg@hotmail.com>
2019-12-18 12:56:36 +0100

    Solved minor issues in the publicKeyFacade

--- src/java/litfistserver/service/PublicKeyFacadeREST.java
+++ src/java/litfistserver/service/PublicKeyFacadeREST.java
@@ -1,33 +1,21 @@
 package litfistserver.service;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.InternalServerErrorException;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import litfitsserver.ejbs.LocalPublicKeyEJB;
-import litfitsserver.entities.Material;
-import litfitsserver.exceptions.CreateException;
-import litfitsserver.exceptions.DeleteException;
-import litfitsserver.exceptions.ReadException;
-import litfitsserver.exceptions.UpdateException;
 
 /**
- * RESTful class for the Material entity
+ * RESTful class for getting the Public Key
  *
  * @author Carlos
  */
-@Path("litfitsserver.entities.material")
+@Path("litfitsserver.encryption.publicKey")
 public class PublicKeyFacadeREST {
     /**
      * Injects the EJB of the entity in question
@@ -39,17 +27,14 @@ public class PublicKeyFacadeREST {
     /**
      * Gets the material wanted
      *
-     * @param name
      * @return Material
      */
     @GET
-    @Path("publicKey")
     @Produces({MediaType.APPLICATION_XML})
     public byte[] getPublicKey() {
-        LOG.info("Giving the public key to a client");
         byte[] publicKey = null;
         try {
-            LOG.info("Finding a Material");
+            LOG.info("Giving the public key to a client");
             publicKey = publicKeyEJB.getPublicKey();
         } catch (IOException ex) {
             LOG.severe(ex.getMessage());
