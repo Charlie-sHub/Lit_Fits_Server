3c6a5e569a19565e18d3b1ea615a0ced20884c06
Asier <@.>
2020-01-24 10:45:19 +0100

    User changes

--- src/java/litfitsserver/service/UserFacadeREST.java
+++ src/java/litfitsserver/service/UserFacadeREST.java
@@ -2,7 +2,6 @@ package litfitsserver.service;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
-import java.util.Set;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
